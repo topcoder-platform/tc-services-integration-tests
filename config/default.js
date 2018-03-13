605ae29aa91f65be2e71a5b5bbf99b3c8d6de3b5
Thomas Kranitsas <kranitsasthomas@gmail.com>
2018-03-13 23:02:07 +0200

    Add support for SMTP, add gitignore, add lint check

--- config/default.js
+++ config/default.js
@@ -2,13 +2,38 @@
  * Default configuration
  */
 
+// Convert string to boolean
+const toBoolean = s => s && s.toString().toLowerCase() === 'true';
+
 module.exports = {
   SAMPLE_TITLE: 'Discussion for a topic', // Sample title to be used for a creating a topic
   SAMPLE_BODY: 'Sample Discussion body for a topic', // Sample body to be used for a creating a topic body
   MODIFIED_TITLE: 'Modified discussion for a topic', // Sample title to be used for a updating a topic
   MODIFIED_BODY: 'Modified sample discussion body for a topic', // Sample body to be used for a updating a topic body
   USER_TOKEN: process.env.USER_TOKEN, // Any Topcoder user valid JWT token
-  TO_EMAILS: ['sharathkumaranbu@gmail.com', 'sharathkumar.anbu@st.niituniversity.in'],
+  TO_EMAILS: ['kranitsasthomas@gmail.com'],
   MAIL_SUBJECT: 'Test results',
-  MAIL_BODY: '<p>Latest test results available here </p>'
+  MAIL_BODY: '<p>Latest test results available here </p>',
+  SMPT_CONFIG: {
+    host: process.env.SMPT_HOST || '',
+    port: parseInt(process.env.SMPT_PORT, 10) || 465,
+    secure: toBoolean(process.env.SMPT_SECURE) || true, // use TLS
+    auth: {
+      user: process.env.SMPT_USER || '',
+      pass: process.env.SMPT_PASS || ''
+    },
+    tls: {
+      // do not fail on invalid certs
+      rejectUnauthorized: toBoolean(process.env.SMPT_REJECT_UNAUTHORIZED) || false
+    }
+  },
+  // [Option] For usage of Gmail instaed of SMPT
+  GMAIL: {
+    service: 'gmail',
+    auth: {
+      user: process.env.GMAIL_USER || '',
+      pass: process.env.GMAIL_PASSWORD || ''
+    }
+  },
+  PREFER_GMAIL: toBoolean(process.env.PREFER_GMAIL) || false
 };
